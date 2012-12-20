219e99b891a87bb2c97cd87ce08512468c7a5edf
Danil Tashkinov <danil.tashkinov@gmail.com>
2012-12-20 17:57:39 +0400

    replaced named_scope with scope

--- init.rb
+++ init.rb
@@ -1,19 +1,17 @@
 require 'redmine'
 
-Dir[File.join(File.dirname(__FILE__),'vendor','plugins','*')].each do |dir|
-  path = File.join(dir, 'lib')
-  $LOAD_PATH << path
-  Dependencies.load_paths << path
-  Dependencies.load_once_paths.delete(path)
-end
+#Dir[File.join(File.dirname(__FILE__),'vendor','plugins','*')].each do |dir|
+#  path = File.join(dir, 'lib')
+#  $LOAD_PATH << path
+#  Dependencies.load_paths << path
+#  Dependencies.load_once_paths.delete(path)
+#end
 
 # Patches to Ruby, oh my
 require 'stuff_to_do_array_patch'
 
 # Patches to the Redmine core.
-require 'dispatcher'
-
-Dispatcher.to_prepare do
+Rails.configuration.to_prepare do
   require_dependency 'project'
   require_dependency 'issue'
   require_dependency 'user'
@@ -37,9 +35,9 @@ Redmine::Plugin.register :stuff_to_do_plugin do
   url 'https://projects.littlestreamsoftware.com/projects/show/redmine-stuff-to-do'
   author_url 'http://www.littlestreamsoftware.com'
   description "The Stuff To Do plugin allows a user to order and prioritize the issues they are doing into a specific order. It will also allow other privilged users to reorder the user's workload."
-  version '0.4.0'
+  version '0.5.0'
 
-  requires_redmine :version_or_higher => '0.8.0'
+  requires_redmine :version_or_higher => '2.1.0'
 
   settings(:partial => 'settings/stuff_to_do_settings',
            :default => {
