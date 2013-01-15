68cd8fbe78c137ac22b9cffa0ed968832e5b02e3
Danil Tashkinov <danil.tashkinov@gmail.com>
2013-01-15 19:37:40 +0400

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ RedmineApp::Application.routes.draw do
   resource :stuff_to_do, :controller => "stuff_to_do", :only => [] do
     member do
       get 'index'
-      get 'reorder'
+      post 'reorder'
       get 'available_issues'
       get 'time_grid'
       post 'add_to_time_grid'
